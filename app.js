c0801877d3a52cbfdc1919b3b1f630b54afb128e
Felipe Escobar R <pipesco93@gmail.com>
2022-12-21 22:40:15 -0500

    se agrega la estructura base del repo

---
+++ app.js
@@ -0,0 +1,17 @@
+// Se crean las variables necesarias
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//Se da el permiso para la carpeta public
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+// Listen en el pueto 3001 localhost:3001
+app.listen(3001, () => console.log('Servidor corriendo'));
+
+// Se crea la pagina principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
