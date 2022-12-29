1b62aa982e819259fb2e84a09fb2548fc8c674c5
rosariobeccarvarela <beccarvarelarosario@gmail.com>
2022-12-29 17:55:24 -0300

    log-in

--- app.js
+++ app.js
@@ -2,16 +2,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+ 
 //Se da el permiso para la carpeta public
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
-
-// Listen en el pueto 3001 localhost:3001
-app.listen(3001, () => console.log('Servidor corriendo'));
-
-// Se crea la pagina principal
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+ 
+// Listen en el puerto 3001 localhost:3001
+app.listen(3001, () => {
+    console.log('Servidor corriendo')
 });
+
+// Se crea el log in
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
 
