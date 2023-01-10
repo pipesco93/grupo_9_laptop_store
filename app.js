82e6ef1b0e4272ba9f0cc4459338228816c8e926
MattiasMG <matias.munozgader@gmail.com>
2023-01-10 00:46:28 -0300

    Register

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 // Listen en el pueto 3001 localhost:3001
-app.listen(3001, () => console.log('Servidor corriendo'));
+app.listen(3002, () => console.log('Servidor corriendo'));
 
 // Se crea la pagina principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+    res.sendFile(path.join(__dirname, './views/register.html'));
 });
 
