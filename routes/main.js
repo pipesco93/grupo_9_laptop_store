17dd6a33b612e4c96dfff53999c6b474d6608dea
Felipe Escobar R <pipesco93@gmail.com>
2023-02-13 20:10:59 -0500

    Borrado de carpetas

--- routes/main.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routerMain = express.Router();
-const maincontroller = require('../controllers/mainController.js')
-
-
-routerMain.get('/', maincontroller.index);
-
-module.exports = routerMain;
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routerUsers = express.Router();
-const userController = require('../controllers/usersController.js')
-
-
-routerUsers.get('/register', userController.register);
-routerUsers.get('/login', userController.login);
-
-module.exports = routerUsers;
