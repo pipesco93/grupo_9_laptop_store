d72cb6fdc6216d6d6b4ad059901354928818f0ed
Felipe Escobar R <pipesco93@gmail.com>
2023-02-13 20:03:27 -0500

    Se crrige eorror de rutas en controller para acceder a archivo json

---
+++ src/app.js
@@ -0,0 +1,36 @@
+// Se crean las variables necesarias
+const express = require('express');
+const app = express();
+const methodOverride = require ('method-override')
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+app.use(methodOverride('_method'))
+
+//pra que pueda leer put y delete
+app.use(express.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 3001;
+
+
+
+//Set ejs
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+// Se requiere ysesa el archivo main de la carpeta router, que es dondese encuentran todas las direcciones del sitio
+const routerMain = require('./routes/main')
+app.use(routerMain);
+
+const routerUsers = require('./routes/users.js')
+app.use(routerUsers);
+
+const routerProducts = require('./routes/products.js')
+app.use(routerProducts);
+
+
+// Listen en el puerto 3001 localhost:3001
+app.listen(port, () => {
+    console.log(`Servidor corriendo ${port}`)
+});
+//prueba
---
+++ src/routes/products.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const routerProducts = express.Router();
+const productsController = require('../controllers/productsController')
+const path = require('path');
+const multer = require('multer');
+
+// Configuraciones de multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../public/images/products/'))
+
+    },
+    filename: (req, file, cb) => {
+        const newProductEdit = `product-${Date.now()}_img${path.extname(file.originalname)}`
+        cb(null, newProductEdit)
+    }
+})
+
+// Instancio multer
+const upload = multer({storage});
+
+routerProducts.get('/products', productsController.products);
+routerProducts.get('/products/:id', productsController.prodDetails);
+routerProducts.get('/cart', productsController.cart);
+routerProducts.get('/product-create', productsController.prodCreate);
+routerProducts.post('/confirm-create', upload.single('image') ,productsController.confirmCreate);
+routerProducts.get('/product-edit/:id' , productsController.productEdit);
+routerProducts.put('/product-edit/:id' , productsController.editConfirm);
+routerProducts.delete('/product-delete/:id' , productsController.prodDelete);
+
+module.exports = routerProducts;
