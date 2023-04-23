7725a75752aa1dbaebe39cde64c5453f87b2cc0a
rosariobeccarvarela <beccarvarelarosario@gmail.com>
2023-04-23 14:02:21 -0300

    Validacion de front end login

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   "development": {
     "username": "root",
-    "password": process.env.PASSWORD,
+    "password": '',
     "database": "laptop_store",
     "host": "127.0.0.1",
     "dialect": "mysql"
